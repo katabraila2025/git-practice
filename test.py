0756a089e557dc2a6be846dbae4521dbd479bd0f
katabraila2025 <katabraila2011@gmail.com>
2026-02-19 07:17:59 +0200

    A doua modificare

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 class TransportExpress(StrategieTransport):
     def calculeaza_cost(self,comanda:Comanda) -> float:
         return 20 + 0.8*comanda.distanta_km + 1.8* comanda.greutate_kg
+
+# De ce metodele abstracte sunt de tipul pass
+class CalculatorTransport:
+    def __init__(self, strategie:StrategieTransport):
+        self.strategie = strategie
+    def seteaza_strategia(self, strategie:StrategieTransport):
+        self.strategie = strategie
+    def cost(self,comanda:Comanda) -> float:
+        return round(self.strategie.calculeaza_cost(comanda),2)
